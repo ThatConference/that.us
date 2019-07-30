d20faac03ad1f617c0e1d220228f131c7bc31b03
Clark Sell <clark.sell@csell.net>
2019-07-30 11:25:48 -0500

    forcing build

--- www/pages/index.js
+++ www/pages/index.js
@@ -1,29 +1,28 @@
 import { Fragment } from 'react';
 import styled from 'styled-components';
 import { ParallaxProvider } from 'react-scroll-parallax';
-import ContentSection from '../components/ContentSection'
-import Hero from '../components/Hero'
-import IconText from '../components/IconText'
-import SocialLinks from '../components/SocialLinks'
-import React, { Component} from 'react';
-import {above, below} from '../utitlies/breakpoint.js';
-
+import ContentSection from '../components/ContentSection';
+import Hero from '../components/Hero';
+import IconText from '../components/IconText';
+import SocialLinks from '../components/SocialLinks';
+import React, { Component } from 'react';
+import { above, below } from '../utitlies/breakpoint.js';
 
 class home extends Component {
   getImage() {
     const items = [
-      "https://live.staticflickr.com/1831/43151326755_0c028664fb_h.jpg",
-      "https://live.staticflickr.com/4429/36422307826_2699e07837_h.jpg",
-      "https://live.staticflickr.com/1812/43359764724_38b9d3a22b_h.jpg",
-      "https://live.staticflickr.com/4423/36299960852_0eee57ccab_h.jpg",
+      'https://live.staticflickr.com/1831/43151326755_0c028664fb_h.jpg',
+      'https://live.staticflickr.com/4429/36422307826_2699e07837_h.jpg',
+      'https://live.staticflickr.com/1812/43359764724_38b9d3a22b_h.jpg',
+      'https://live.staticflickr.com/4423/36299960852_0eee57ccab_h.jpg'
     ];
-    return items[Math.floor(Math.random() * items.length)]
+    return items[Math.floor(Math.random() * items.length)];
   }
 
   componentDidMount() {
-    const script = document.createElement("script");
+    const script = document.createElement('script');
 
-    script.src = "https://thatconference.activehosted.com/f/embed.php?id=10";
+    script.src = 'https://thatconference.activehosted.com/f/embed.php?id=10';
     script.async = true;
 
     document.body.appendChild(script);
@@ -37,9 +36,9 @@ class home extends Component {
             imagePath={this.getImage()}
             fullHeight="true"
             heading="THAT.us"
-            subheading="it's not a you, me or them it is US all, together"
+            subheading="it's not a you, me, or them, it is US all, together"
           >
-            <div class="_form_10"></div>
+            <div class="_form_10" />
             <SocialLinks />
           </Hero>
         </Fragment>
