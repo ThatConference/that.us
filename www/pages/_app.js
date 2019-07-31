fea554c2a7e92ae24ace033a3fbdd868e1077648
Clark Sell <clark.sell@csell.net>
2019-07-31 08:11:13 -0500

    updating how global styles render

--- www/pages/_app.js
+++ www/pages/_app.js
@@ -3,16 +3,6 @@ import App, { Container } from 'next/app';
 import Page from '../components/Page';
 
 export default class MyApp extends App {
-  static async getInitialProps({ Component, router, ctx }) {
-    let pageProps = {};
-
-    if (Component.getInitialProps) {
-      pageProps = await Component.getInitialProps(ctx);
-    }
-
-    return { pageProps };
-  }
-
   render() {
     const { Component, pageProps } = this.props;
 
