23b0929d5ce8414338297b75035272246badcbac
Clark Sell <clark.sell@csell.net>
2020-07-13 14:46:57 -0500

    updates a to link

--- src/routes/sessions/SessionDetails.svelte
+++ src/routes/sessions/SessionDetails.svelte
@@ -108,7 +108,7 @@
 
           {#if canJoin()}
             <span class="ml-3 inline-flex rounded-md shadow-sm">
-              <a
+              <Link
                 type="button"
                 href="/join/{sessionId}"
                 class="relative inline-flex items-center px-4 py-2 border
@@ -123,7 +123,7 @@
                   class="-ml-1 mr-2 h-5 w-5 text-gray-400"
                 />
                 <span>Join In</span>
-              </a>
+              </Link>
             </span>
           {/if}
         </div>
