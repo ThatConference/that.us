806f04a0ff94c6f0ed17a7300d30b3f10817d2ed
Clark Sell <clark.sell@csell.net>
2020-07-15 14:06:10 -0500

    adds faq page

---
+++ src/components/faq/FaqItem.svelte
@@ -0,0 +1,39 @@
+<script>
+  import items from './items.json';
+</script>
+
+<div class="mt-6 border-t-2 border-gray-200 pt-6">
+  <dl>
+
+    {#each items as faq, i}
+      {#if i === 0}
+        <div class="md:grid md:grid-cols-12 md:gap-8">
+          <dt
+            class="text-base leading-6 font-medium text-gray-900 md:col-span-5"
+          >
+            {faq.question}
+          </dt>
+
+          <dd class="mt-2 md:mt-0 md:col-span-7">
+            <p class="text-base leading-6 text-gray-500">{faq.answer}</p>
+          </dd>
+        </div>
+      {:else}
+        <div
+          class="mt-8 border-t border-gray-200 pt-6 md:grid md:grid-cols-12
+          md:gap-8"
+        >
+          <dt
+            class="text-base leading-6 font-medium text-gray-900 md:col-span-5"
+          >
+            {faq.question}
+          </dt>
+          <dd class="mt-2 md:mt-0 md:col-span-7">
+            <p class="text-base leading-6 text-gray-500">{faq.answer}</p>
+          </dd>
+        </div>
+      {/if}
+    {/each}
+
+  </dl>
+</div>
---
+++ src/routes/FAQ.svelte
@@ -0,0 +1,28 @@
+<script>
+  import Nav from '../components/home/nav/MainNav.svelte';
+
+  import FaqItems from '../components/faq/FaqItem.svelte';
+</script>
+
+<div class="relative bg-white overflow-hidden">
+  <div class="max-w-screen-xl mx-auto ">
+
+    <div class="grid grid-cols-1 gap-8">
+      <Nav />
+
+      <div class="bg-gray-50">
+        <div
+          class="max-w-screen-xl mx-auto py-12 px-4 sm:px-6 lg:py-16 lg:px-8"
+        >
+          <h2
+            class="text-3xl leading-9 font-extrabold text-gray-900 sm:text-4xl
+            sm:leading-10"
+          >
+            Frequently asked questions
+          </h2>
+          <FaqItems />
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
