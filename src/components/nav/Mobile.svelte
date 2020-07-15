904c733b8c1ef0d4a1d2f9df15ac06c0ca3f9cf6
Clark Sell <clark.sell@csell.net>
2020-07-14 19:19:11 -0500

    spelling is hard

--- src/components/nav/Mobile.svelte
+++ src/components/nav/Mobile.svelte
@@ -78,7 +78,7 @@
     >
       {#if $isAuthenticated}
         <Link
-          href="/my/favorties"
+          href="/my/favorites"
           class="block px-3 py-2 rounded-md text-base font-medium text-gray-400
           hover:text-white hover:bg-gray-700 focus:outline-none focus:text-white
           focus:bg-gray-700"
