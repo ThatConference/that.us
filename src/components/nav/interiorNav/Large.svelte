5f9c3dbb8c384a7aae27bc988b6d47817931a836
Clark Sell <clark.sell@csell.net>
2020-07-30 09:04:10 -0500

    adds the shell of the help page

--- src/components/nav/interiorNav/Large.svelte
+++ src/components/nav/interiorNav/Large.svelte
@@ -68,6 +68,14 @@
       >
         FAQs
       </Link>
+      <Link
+        href="/help"
+        class="ml-4 px-3 py-2 rounded-md text-sm font-medium text-gray-300
+        hover:text-white hover:bg-gray-700 focus:outline-none focus:text-white
+        focus:bg-gray-700"
+      >
+        Help
+      </Link>
     </div>
   </div>
 </div>
