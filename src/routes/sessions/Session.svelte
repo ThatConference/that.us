b7a14fa0d1183bc37bf52d1bd1b260c8d84da215
Clark Sell <clark.sell@csell.net>
2020-08-08 07:43:01 -0500

    fixes #263

--- src/routes/sessions/Session.svelte
+++ src/routes/sessions/Session.svelte
@@ -24,6 +24,7 @@
               shortDescription
               tags
               startTime
+              durartionInMinutes
               speakers {
                 firstName
                 lastName
