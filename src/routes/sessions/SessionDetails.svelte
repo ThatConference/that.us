e170356eab04d558eba33452bea00d4c89a28259
Clark Sell <clark.sell@csell.net>
2020-08-13 12:01:12 -0500

    helps if you have the href

--- src/routes/sessions/SessionDetails.svelte
+++ src/routes/sessions/SessionDetails.svelte
@@ -229,6 +229,7 @@
               <span class="ml-3 inline-flex rounded-md shadow-sm">
                 <Link
                   type="button"
+                  href="/join/{sessionId}"
                   class="relative inline-flex items-center px-4 py-2 border
                   border-gray-300 text-sm leading-5 font-medium rounded-md
                   text-gray-700 bg-white hover:text-gray-500 focus:outline-none
