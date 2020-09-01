489091ae90d97bf437be39fd279143803fe66395
Clark Sell <clark.sell@csell.net>
2020-09-01 09:54:08 -0500

    cleaning up tailwind purgecss

--- purgecss.config.js
+++ purgecss.config.js
@@ -1,7 +1,6 @@
 /* eslint-disable no-undef */
-/* eslint-disable svelte3/lint-template */
 module.exports = {
   content: ['./src/index.html', './src/**/*.svelte'],
   whitelistPatterns: [/svelte-/],
-  defaultExtractor: (content) => content.match(/[A-Za-z0-9-_:/]+/g) || [],
+  defaultExtractor: content => content.match(/[A-Za-z0-9-_:/]+/g) || [],
 };
