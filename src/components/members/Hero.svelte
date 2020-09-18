0efbae2448a9516ec419a1c8e73ad544af37d83e
asharonbaltazar <asharonbaltazar@outlook.com>
2020-09-18 09:47:28 -0500

    Removed div with 'mx-auto' & 'max-w-screen-xl'

--- src/components/members/Hero.svelte
+++ src/components/members/Hero.svelte
@@ -6,60 +6,58 @@
 </script>
 
 <div class="relative bg-white overflow-hidden">
-  <div class="max-w-screen-xl mx-auto">
-    <div
-      class="absolute z-10 pb-8 bg-white sm:pb-16 md:pb-20 lg:max-w-2xl
-      lg:w-full lg:pb-28 xl:pb-32"
+  <div
+    class="absolute z-10 pb-8 bg-white sm:pb-16 md:pb-20 lg:max-w-2xl
+    lg:w-full lg:pb-28 xl:pb-32"
+  >
+    <Nav />
+
+    <main
+      class="mt-24 mx-auto max-w-screen-xl px-4 sm:mt-12 sm:px-6 md:mt-16
+      lg:mt-20 lg:px-8 xl:mt-28"
     >
-      <Nav />
+      <div class="sm:text-center lg:text-left">
+        <h2
+          class="text-4xl tracking-tight leading-10 font-extrabold
+          text-thatBlue-800 sm:text-5xl sm:leading-none md:text-6xl"
+        >
+          Welcome to
+          <br />
+          <span class="text-thatOrange-400">THAT Family</span>
+        </h2>
+        <p
+          class="mt-3 text-base text-gray-500 sm:mt-5 sm:text-lg sm:max-w-xl
+          sm:mx-auto md:mt-5 md:text-xl lg:mx-0"
+        >
+          At the heart of everything we do, you can find our THAT Community
+          Members. We absolutely love our Members and welcome each and every
+          one. Each Member brings a unique skill set and background that
+          enriches our conversations and relationships. If you haven’t already
+          signed up to become a member, we’d love it if you did!
+        </p>
 
-      <main
-        class="mt-24 mx-auto max-w-screen-xl px-4 sm:mt-12 sm:px-6 md:mt-16
-        lg:mt-20 lg:px-8 xl:mt-28"
-      >
-        <div class="sm:text-center lg:text-left">
-          <h2
-            class="text-4xl tracking-tight leading-10 font-extrabold
-            text-thatBlue-800 sm:text-5xl sm:leading-none md:text-6xl"
+        {#if !$isAuthenticated}
+          <div
+            class="mt-5 sm:mt-8 sm:flex sm:justify-center lg:justify-start"
           >
-            Welcome to
-            <br />
-            <span class="text-thatOrange-400">THAT Family</span>
-          </h2>
-          <p
-            class="mt-3 text-base text-gray-500 sm:mt-5 sm:text-lg sm:max-w-xl
-            sm:mx-auto md:mt-5 md:text-xl lg:mx-0"
-          >
-            At the heart of everything we do, you can find our THAT Community
-            Members. We absolutely love our Members and welcome each and every
-            one. Each Member brings a unique skill set and background that
-            enriches our conversations and relationships. If you haven’t already
-            signed up to become a member, we’d love it if you did!
-          </p>
-
-          {#if !$isAuthenticated}
-            <div
-              class="mt-5 sm:mt-8 sm:flex sm:justify-center lg:justify-start"
-            >
-              <div class="rounded-md shadow">
-                <Link
-                  href="/login?signup=true"
-                  class="w-full flex items-center justify-center px-8 py-3
-                  border border-transparent text-base leading-6 font-medium
-                  rounded-md text-white bg-thatOrange-400
-                  hover:bg-thatOrange-500 focus:outline-none
-                  focus:border-thatOrange-700 focus:shadow-outline-that-orange
-                  transition duration-150 ease-in-out md:py-4 md:text-lg
-                  md:px-10"
-                >
-                  Sign up today!
-                </Link>
-              </div>
+            <div class="rounded-md shadow">
+              <Link
+                href="/login?signup=true"
+                class="w-full flex items-center justify-center px-8 py-3
+                border border-transparent text-base leading-6 font-medium
+                rounded-md text-white bg-thatOrange-400
+                hover:bg-thatOrange-500 focus:outline-none
+                focus:border-thatOrange-700 focus:shadow-outline-that-orange
+                transition duration-150 ease-in-out md:py-4 md:text-lg
+                md:px-10"
+              >
+                Sign up today!
+              </Link>
             </div>
-          {/if}
-        </div>
-      </main>
-    </div>
+          </div>
+        {/if}
+      </div>
+    </main>
   </div>
   <div class="lg:absolute lg:inset-y-0 lg:right-0 lg:w-1/2">
     <img
