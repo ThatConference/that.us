8c4faea5522411f89d51d93b22065a470e24fcb6
Clark Sell <clark.sell@csell.net>
2020-09-21 19:15:32 -0500

    redirects old sessions route

---
+++ src/routes/SessionRedirect.svelte
@@ -0,0 +1,6 @@
+<script>
+  import { navigateTo } from 'yrv';
+
+  let currentPath = window.location.pathname.replace('sessions', 'activities');
+  navigateTo(`${currentPath}${window.location.search}`);
+</script>
