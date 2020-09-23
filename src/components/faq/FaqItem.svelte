265b93be926a3aea22d2dd49d352680970eacd91
Clark Sell <clark.sell@csell.net>
2020-09-23 18:48:41 -0500

    moves the search to the header, updates references

--- src/components/faq/FaqItem.svelte
+++ src/components/faq/FaqItem.svelte
@@ -1,54 +1,10 @@
 <script>
-  import items from './items.json';
-  import Fuse from 'fuse.js';
-
-  // https://fusejs.io/api/options.html
-  const options = {
-    // isCaseSensitive: false,
-    includeScore: true,
-    //shouldSort: false,
-    // includeMatches: false,
-    findAllMatches: true,
-    // minMatchCharLength: 1,
-    // location: 0,
-    threshold: 0.2,
-    // distance: 100,
-    // useExtendedSearch: false,
-    ignoreLocation: true,
-    // ignoreFieldNorm: false,
-    keys: ['question', 'answer'],
-  };
-  let searchterm = '';
-  let items2;
-  const fuse = new Fuse(items, options);
-
-  function filter(term) {
-    if (term === '') {
-      items2 = items;
-      return;
-    }
-    items2 = fuse.search(term).map(r => ({
-      question: r.item.question,
-      answer: r.item.answer,
-      category: r.item.category,
-    }));
-    console.log(items2);
-  }
-  filter(searchterm);
+  export let faqs = [];
 </script>
 
 <div class="mt-6 border-t-2 border-gray-200 pt-6">
-  <span>Filter for:</span>
-  <input
-    class="form-input"
-    bind:value="{searchterm}"
-    on:input="{filter(searchterm)}"
-  />
-</div>
-<div class="mt-6 border-t-2 border-gray-200 pt-6">
   <dl>
-
-    {#each items2 as faq, i}
+    {#each faqs as faq, i}
       {#if i === 0}
         <div class="md:grid md:grid-cols-12 md:gap-8">
           <dt
@@ -66,7 +22,7 @@
       {:else}
         <div
           class="mt-8 border-t border-gray-200 pt-6 md:grid md:grid-cols-12
-          md:gap-8"
+            md:gap-8"
         >
           <dt
             class="text-base leading-6 font-medium text-gray-900 md:col-span-5"
@@ -81,7 +37,6 @@
         </div>
       {/if}
     {/each}
-
   </dl>
 </div>
 
--- src/routes/support/FAQ.svelte
+++ src/routes/support/FAQ.svelte
@@ -1,10 +1,11 @@
 <script>
-  import Layout from '../../elements/layouts/ContentLayout.svelte';
-  import Nav from '../../components/nav/mainNav/MainNav.svelte';
-  import FaqItems from '../../components/faq/FaqItem.svelte';
-  import { Footer } from '../../components';
+  import Fuse from 'fuse.js';
 
+  import Layout from '../../elements/layouts/ContentLayout.svelte';
+  import FaqItems from '../../components/faq/FaqItem.svelte';
+  import items from './faqItems.json';
   import metaTagsStore from '../../store/metaTags';
+
   metaTagsStore.set({
     title: 'FAQ - THAT',
     description: 'A common list of frequently asked questions, answered.',
@@ -13,31 +14,80 @@
       url: `https://that.us/support/faq`,
     },
   });
+
+  // https://fusejs.io/api/options.html
+  const options = {
+    // isCaseSensitive: false,
+    includeScore: true,
+    //shouldSort: false,
+    // includeMatches: false,
+    findAllMatches: true,
+    // minMatchCharLength: 1,
+    // location: 0,
+    threshold: 0.2,
+    // distance: 100,
+    // useExtendedSearch: false,
+    ignoreLocation: true,
+    // ignoreFieldNorm: false,
+    keys: ['question', 'answer'],
+  };
+
+  let searchterm = '';
+  let itemsFound;
+  const fuse = new Fuse(items, options);
+
+  function filter(term) {
+    if (term === '') {
+      itemsFound = items;
+      return;
+    }
+
+    itemsFound = fuse.search(term).map(r => ({
+      question: r.item.question,
+      answer: r.item.answer,
+      category: r.item.category,
+    }));
+  }
+
+  filter(searchterm);
 </script>
 
 <Layout>
-  <main class="overflow-hidden">
-    <div class="relative pb-16 md:pb-20 lg:pb-24 xl:pb-32">
-      <div class="mt-32 mx-auto max-w-screen-xl px-4 sm:px-6 xl:mt-40">
-        <div class="sm:text-center lg:text-left">
-          <h2
-            class="text-4xl tracking-tight leading-10 font-extrabold
-            text-thatBlue-800 sm:text-5xl sm:leading-none md:text-6xl"
-          >
-            Frequently Asked Questions
-          </h2>
-          <p
-            class="mt-3 text-base text-gray-500 sm:mt-5 sm:text-lg sm:max-w-xl
-            sm:mx-auto md:mt-5 md:text-xl lg:mx-0"
-          >
-            A common list of frequently asked questions, answered.
-          </p>
-        </div>
+  <div class="py-16 overflow-hidden">
+    <div class="relative max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+      <div class="text-base max-w-prose mx-auto lg:max-w-none">
+        <div
+          class="flex justify-between items-center flex-wrap sm:flex-no-wrap
+            flex-col md:flex-row"
+        >
+          <div>
+            <p
+              class="text-base leading-6 text-thatOrange-400 font-semibold
+                tracking-wide uppercase"
+            >
+              Help && Support
+            </p>
+            <h1
+              class="mt-2 mb-8 text-3xl leading-8 font-extrabold tracking-tight
+                text-gray-900 sm:text-4xl sm:leading-10"
+            >
+              Frequently Asked Questions
+            </h1>
+          </div>
 
-        <div>
-          <FaqItems />
+          <div class="border-gray-200">
+            <input
+              class="form-input"
+              bind:value="{searchterm}"
+              on:input="{filter(searchterm)}"
+              placeholder="type to search..."
+            />
+          </div>
         </div>
       </div>
+      <div>
+        <FaqItems faqs="{itemsFound}" />
+      </div>
     </div>
-  </main>
+  </div>
 </Layout>
