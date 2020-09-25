ad434c26256893746371c196adf72b6e811981bc
Clark Sell <clark.sell@csell.net>
2020-09-25 08:54:00 -0500

    increases padding on content layout to adjust for new nav

--- src/routes/support/_Layout.svelte
+++ src/routes/support/_Layout.svelte
@@ -3,7 +3,7 @@
 </script>
 
 <Layout>
-  <div class="py-16 overflow-hidden">
+  <div class="py-20 overflow-hidden">
     <div class="relative max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
       <slot />
     </div>
