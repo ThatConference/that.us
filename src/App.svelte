72d699031427917c76dedc3a2ae2d36435eb8bfd
Yassine Latreche <59394690+Yassine-Latreche@users.noreply.github.com>
2020-10-05 00:01:35 +0100

    Update App.svelte
    Fixing Tidio Problem

--- src/App.svelte
+++ src/App.svelte
@@ -166,6 +166,19 @@
     // set true to disable Google Analytics
     window['ga-disable-UA-21705613-11'] = window.origin.includes('localhost');
   </script>
+  <script
+    src="https://code.jquery.com/jquery-3.5.1.min.js"
+    integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0="
+    crossorigin="anonymous"></script>
+  <script>
+    // FIXING TIDIO OVERLAY
+    function tidioFixed () {
+      setTimeout(function () {
+        window.$('iframe#tidio-chat-iframe').css('z-index', 5);
+      }, 2500);
+    }
+    tidioFixed();
+  </script>
 </svelte:head>
 
 <main>
