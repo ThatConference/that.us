bfd90bd80fbeb11abdb02a5b038d431faf1d77f1
Clark Sell <clark.sell@csell.net>
2020-10-07 16:23:24 -0500

    build config settings

--- postcss.config.js
+++ postcss.config.js
@@ -1,28 +1,16 @@
 /* eslint-disable global-require */
 /* eslint-disable no-undef */
 
-// const plugins =
-//   process.env.NODE_ENV === 'production'
-//     ? [
-//         require('tailwindcss'),
-//         require('@tailwindcss/ui'),
-//         require('autoprefixer'),
-//         require('@fullhuman/postcss-purgecss'),
-//       ]
-//     : [require('tailwindcss')];
+const tailwindcss = require('tailwindcss');
 
-// module.exports = { plugins };
+const plugins =
+  process.env.NODE_ENV === 'production'
+    ? [
+        tailwindcss('./tailwind.config.js'),
+        require('@tailwindcss/ui'),
+        require('autoprefixer'),
+        require('@fullhuman/postcss-purgecss'),
+      ]
+    : [tailwindcss('./tailwind.config.js')];
 
-const purgecss = require('@fullhuman/postcss-purgecss')({
-  content: ['./src/**/*.html', './src/**/*.svelte'],
-
-  whitelistPatterns: [/svelte-/],
-
-  defaultExtractor: content => content.match(/[A-Za-z0-9-_:/]+/g) || [],
-});
-
-const production = !process.env.ROLLUP_WATCH;
-
-module.exports = {
-  plugins: [require('tailwindcss'), ...(production ? [purgecss] : [])],
-};
+module.exports = { plugins };
