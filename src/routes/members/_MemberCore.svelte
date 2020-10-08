9359ba61e3d0a12a14e87f0639d7b7e80b0d14ff
Clark Sell <clark.sell@csell.net>
2020-10-08 14:28:42 -0500

    closes issue #411 (#412)

--- src/routes/members/_MemberCore.svelte
+++ src/routes/members/_MemberCore.svelte
@@ -1,18 +1,23 @@
 <script>
   export let member;
-
+  
   import { fade } from 'svelte/transition';
-
+  import {navigateTo} from 'yrv';
+  
   import metaTagsStore from '../../store/metaTags';
-
+  
   import NewestFollowers from './_NewestFollowers.svelte';
   import CTA from './_CTA.svelte';
   import UpNext from './_UpNext.svelte';
   import LifeHack from './_LifeHack.svelte';
   import MeritBadges from './_MeritBadges.svelte';
   import Hero from './_Hero.svelte';
-
+  
   import WarningModal from '../../components/notifications/Warning.svelte';
+  
+  if (!member) {
+    navigateTo('/not-found');
+  }
 
   metaTagsStore.set({
     title: `${member.firstName} ${member.lastName} - THAT`,
@@ -37,40 +42,42 @@
   }
 </script>
 
-<div class="flex flex-col">
-  <div in:fade="{{ delay: getDelay() }}">
-    <Hero member="{member}" on:click="{handleFollow}" />
-  </div>
-
-  {#if member.lifeHack}
+{#if member}
+  <div class="flex flex-col">
     <div in:fade="{{ delay: getDelay() }}">
-      <LifeHack
-        quote="{member.lifeHack}"
-        name="{`${member.firstName} ${member.lastName}`}"
-      />
+      <Hero member="{member}" on:click="{handleFollow}" />
     </div>
-  {/if}
 
-  <div in:fade="{{ delay: getDelay() }}">
-    <UpNext activities="{member.sessions}" />
-  </div>
-
-  <div in:fade="{{ delay: getDelay() }}">
-    <CTA memberSlug="{member.profileSlug}" on:click="{handleFollow}" />
-
-    {#if member.earnedMeritBadges.length > 0}
-      <MeritBadges meritBadges="{member.earnedMeritBadges}" />
+    {#if member.lifeHack}
+      <div in:fade="{{ delay: getDelay() }}">
+        <LifeHack
+          quote="{member.lifeHack}"
+          name="{`${member.firstName} ${member.lastName}`}"
+        />
+      </div>
     {/if}
 
-    {#if member.followers && member.followers.length > 0}
-      <NewestFollowers />
+    <div in:fade="{{ delay: getDelay() }}">
+      <UpNext activities="{member.sessions}" />
+    </div>
+
+    <div in:fade="{{ delay: getDelay() }}">
+      <CTA memberSlug="{member.profileSlug}" on:click="{handleFollow}" />
+
+      {#if member.earnedMeritBadges.length > 0}
+        <MeritBadges meritBadges="{member.earnedMeritBadges}" />
+      {/if}
+
+      {#if member.followers && member.followers.length > 0}
+        <NewestFollowers />
+      {/if}
+    </div>
+
+    {#if showModal}
+      <WarningModal
+        on:click="{() => (showModal = false)}"
+        message="Not, yet. We're still working on this feature. Check back soon."
+      />
     {/if}
   </div>
-
-  {#if showModal}
-    <WarningModal
-      on:click="{() => (showModal = false)}"
-      message="Not, yet. We're still working on this feature. Check back soon."
-    />
-  {/if}
-</div>
+{/if}
