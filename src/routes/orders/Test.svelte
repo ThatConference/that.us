d7274c02487da055eb75e6a2d2d71aa17603617b
Clark Sell <clark.sell@csell.net>
2021-01-13 15:37:13 -0600

    adds order summary steps and state machine

---
+++ src/routes/orders/Test.svelte
@@ -0,0 +1,43 @@
+<script>
+  import { Link } from 'yrv';
+  import { useMachine } from 'xstate-svelte';
+
+  import { Standard as StandardButton } from '../../elements/buttons';
+  import { debug } from '../../config';
+  import { cart } from '../../utilities/cart';
+
+  const { state, send } = useMachine($cart, {
+    devTools: debug.xstate,
+  });
+</script>
+
+<div>
+  <StandardButton
+    class="h-3/4"
+    on:click="{() => send('ADD_ITEM', { productId: '1234' })}"
+  >
+    addItem 1
+  </StandardButton>
+
+  <StandardButton
+    class="h-3/4"
+    on:click="{() => send('ADD_ITEM', { productId: '4321', quantity: 2 })}"
+  >
+    addItem 2
+  </StandardButton>
+
+  <StandardButton
+    class="h-3/4"
+    on:click="{() => send('REMOVE_ITEM', { productId: '4321' })}"
+  >
+    remove item
+  </StandardButton>
+
+  <StandardButton class="h-3/4" on:click="{() => send('CLEAR_CART')}">
+    clear storage
+  </StandardButton>
+
+  <div>
+    <Link href="/orders/summary">Order Summary</Link>
+  </div>
+</div>
