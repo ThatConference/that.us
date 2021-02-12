63b9ab968b17dc658c21f1218a184838b74a7d6f
Clark Sell <clark.sell@csell.net>
2021-02-12 14:22:36 -0600

    removing unused component

--- src/components/home/index.js
+++ src/components/home/index.js
@@ -4,6 +4,5 @@ export { default as Testimonials } from './Testimonials.svelte';
 export { default as Newsletter } from './Newsletter.svelte';
 export { default as Sponsors } from './Sponsors.svelte';
 export { default as Stats } from './Stats.svelte';
-export { default as Features } from './Features.svelte';
 export { default as Events } from './Events.svelte';
 export { default as NewMembers } from './NewMembers.svelte';
