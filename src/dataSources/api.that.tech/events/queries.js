95eacd0204c0db9d9209559e8aa55d5b8c6821c6
Clark Sell <clark.sell@csell.net>
2021-02-19 08:33:25 -0600

    all events landing page

--- src/dataSources/api.that.tech/events/queries.js
+++ src/dataSources/api.that.tech/events/queries.js
@@ -17,6 +17,45 @@ export const QUERY_EVENTS = `
         community
         isFeatured
         isActive
+        theme { 
+          heroSlug
+        }
+        venues {
+          city
+          state
+        }
+      }
+    }
+  }
+`;
+
+export const QUERY_EVENTS_BY_COMMUNITY = `
+  query QUERY_EVENTS_BY_COMMUNITY ($slug: Slug) {
+    communities {
+      community (findBy: {slug: $slug}) {
+        get {
+          events {
+            id
+            name
+            description
+            slogan
+            type  
+            startDate
+            endDate
+            year
+            slug
+            community
+            isFeatured
+            isActive
+            theme { 
+              heroSlug
+            }
+            venues {
+              city
+              state
+            }
+          }  
+        }
       }
     }
   }
@@ -39,5 +78,21 @@ export default client => {
       });
   }
 
-  return { queryEvents };
+  function queryEventsByCommunity(slug = 'that') {
+    const variables = { slug };
+
+    return client
+      .query(QUERY_EVENTS_BY_COMMUNITY, variables, {
+        fetchOptions: { headers: { ...stripAuthorizationHeader(client) } },
+      })
+      .toPromise()
+      .then(({ data, error }) => {
+        if (error) log(error, 'QUERY_EVENTS');
+
+        const { community } = data.communities;
+        return community ? community.get.events : [];
+      });
+  }
+
+  return { queryEvents, queryEventsByCommunity };
 };
---
+++ src/routes/events/components/_EventsHero.svelte
@@ -0,0 +1,22 @@
+<div class="sm:text-center lg:text-left">
+  <h2
+    class="text-4xl tracking-tight leading-10 font-extrabold text-thatBlue-800 sm:text-5xl sm:leading-none md:text-6xl">
+    Events at
+    <span class="text-thatOrange-400">THAT</span>
+  </h2>
+  <p
+    class="mt-3 text-base text-gray-500 sm:mt-5 sm:text-lg sm:mx-auto md:mt-5 md:text-xl lg:mx-0">
+    Bacon ipsum dolor amet tenderloin pig shank, jerky kielbasa shoulder corned
+    beef chuck t-bone. Prosciutto bresaola chuck venison fatback. Prosciutto
+    venison bresaola, biltong short ribs capicola cupim burgdoggen pastrami pork
+    chop ribeye ball tip pancetta. Pork chop beef salami, turducken filet mignon
+    tail bacon boudin. Interested in supporting us? Email us at
+    <a
+      href="mailto:hello@that.us?subject=Let's Partner"
+      rel="noopener"
+      target="_blank"
+      class="text-thatOrange-400 hover:text-thatOrange-500 hover:underline"
+      >hello@that.us</a
+    >.
+  </p>
+</div>
