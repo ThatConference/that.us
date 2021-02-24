bee195927454c841bd12ad35d67c170d9980f4cb
Clark Sell <clark.sell@csell.net>
2021-02-24 10:23:59 -0600

    adds faq copy

--- src/components/faq/_FAQs.svelte
+++ src/components/faq/_FAQs.svelte
@@ -6,10 +6,10 @@
 
 <div class="relative">
   <div class="max-w-7xl mx-auto py-16 px-4 sm:px-6 lg:py-20 lg:px-8">
-    <div class="lg:grid lg:grid-cols-3 lg:gap-8">
-      <div class="space-y-4">
+    <div class="lg:grid lg:grid-cols-5 lg:gap-16">
+      <div class="lg:col-span-2 space-y-4">
         <h2 class="text-3xl font-extrabold text-thatBlue-800">
-          Frequently asked questions
+          Frequently Asked Questions
         </h2>
         <p class="text-lg text-gray-500">
           Can’t find the answer you’re looking for? Feel free to
@@ -20,7 +20,8 @@
           or reach out in the chat.
         </p>
       </div>
-      <div class="mt-12 lg:mt-0 lg:col-span-2">
+
+      <div class="mt-12 lg:mt-0 lg:col-span-3 lg:ml-16">
         <dl class="space-y-12">
           <div>
             {#each faqs as faq}
