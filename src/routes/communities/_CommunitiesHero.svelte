d3132075901d96a9d1649e8b664e55254637509f
Clark Sell <clark.sell@csell.net>
2021-02-25 14:30:18 -0600

    adds event sponsors page

--- src/routes/communities/_CommunitiesHero.svelte
+++ src/routes/communities/_CommunitiesHero.svelte
@@ -1,15 +1,13 @@
 <div class="sm:text-center lg:text-left">
   <h2
     class="text-4xl tracking-tight leading-10 font-extrabold text-thatBlue-800
-      sm:text-5xl sm:leading-none md:text-6xl"
-  >
+      sm:text-5xl sm:leading-none md:text-6xl">
     Communities at
     <span class="text-thatOrange-400">THAT</span>
   </h2>
   <p
     class="mt-3 text-base text-gray-500 sm:mt-5 sm:text-lg sm:max-w-xl
-      sm:mx-auto md:mt-5 md:text-xl lg:mx-0"
-  >
+      sm:mx-auto md:mt-5 md:text-xl lg:mx-0">
     At THAT our mission is to help communities grow. Regardless of your
     interests or where you call home we believe there is something special when
     a group of Geeks or Geeklings can get together and share with one another. A
@@ -17,10 +15,11 @@
     geeking out over some hot technology stack. Interested in running a
     community here? Email us at
     <a
-      href="mailto:hello@thatconference.com?subject=Adding a new community to THAT.us"
+      href="mailto:hello@that.us?subject=Adding a new community to THAT.us"
       rel="noopener"
       target="_blank"
       class="text-thatOrange-400 hover:text-thatOrange-500 hover:underline"
-    >hello@thatconference.com</a>.
+      >hello@that.us</a
+    >.
   </p>
 </div>
