2c5686c4317a69b59732171328d9f944e70953e3
Clark Sell <clark.sell@csell.net>
2021-03-24 10:18:30 -0500

    reworks the header layout

--- src/components/members/Hero.svelte
+++ src/components/members/Hero.svelte
@@ -8,15 +8,16 @@
 <div class="sm:text-center lg:text-left">
   <h2
     class="text-4xl tracking-tight leading-10 font-extrabold text-thatBlue-800
-      sm:text-5xl sm:leading-none md:text-6xl"
-  >
+      sm:text-5xl sm:leading-none md:text-6xl">
     Welcome to
     <span class="text-thatOrange-400">THAT Family</span>
   </h2>
+
   <p
-    class="mt-3 text-base text-gray-500 sm:mt-5 sm:text-lg sm:max-w-xl
-      sm:mx-auto md:mt-5 md:text-xl lg:mx-0"
-  >
+    class="mt-3 sm:mt-5 md:mt-5 sm:mx-auto lg:mx-0
+    text-base text-gray-500  
+    sm:text-lg md:text-xl
+    sm:max-w-5xl">
     At the heart of everything we do, you can find our THAT Community Members.
     We absolutely love our Members and welcome each and every one. Each Member
     brings a unique skill set and background that enriches our conversations and
@@ -34,8 +35,7 @@
             text-white bg-thatOrange-400 hover:bg-thatOrange-500
             focus:outline-none focus:border-thatOrange-700
             focus:ring-that-orange transition duration-150 ease-in-out
-            md:py-4 md:text-lg md:px-10"
-        >
+            md:py-4 md:text-lg md:px-10">
           Sign up today!
         </Link>
       </div>
