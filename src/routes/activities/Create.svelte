3df63fb86e86cf930f3f2fd51f355340ae044116
Clark Sell <clark.sell@csell.net>
2021-03-24 12:40:06 -0500

    closes #609 and closes #631

--- src/routes/activities/Create.svelte
+++ src/routes/activities/Create.svelte
@@ -9,6 +9,7 @@
   // UI Support
   import StackedLayout from '../../elements/layouts/StackedLayout.svelte';
   import { ActionHeader } from '../../elements';
+  import { Standard as StandardLink } from '../../elements/links';
   import Nav from '../../components/nav/interiorNav/Top.svelte';
   import ActivityForm from './components/form/ActivityForm.svelte';
 
@@ -92,12 +93,9 @@
       </div>
       <div
         class="mt-6 flex flex-col-reverse justify-stretch space-y-4 space-y-reverse sm:flex-row-reverse sm:justify-end sm:space-x-reverse sm:space-y-0 sm:space-x-3 md:mt-0 md:flex-row md:space-x-3">
-        <Link
-          href="/my/submissions"
-          type="button"
-          class="inline-flex items-center justify-center px-4 py-2 border border-transparent text-sm font-medium rounded-md shadow-sm text-white bg-blue-600 hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-100 focus:ring-blue-500">
+        <StandardLink href="/my/submissions">
           View Your Past Submissions
-        </Link>
+        </StandardLink>
       </div>
     </div>
 
