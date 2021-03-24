e785f15c0b6fc9ccd11a83d863107d8fd22b1b8c
Clark Sell <clark.sell@csell.net>
2021-03-24 10:18:30 -0500

    reworks the header layout

--- src/components/partners/Hero.svelte
+++ src/components/partners/Hero.svelte
@@ -1,10 +1,14 @@
 <section>
   <h2
-    class="text-4xl tracking-tight leading-10 font-extrabold text-thatBlue-800 sm:text-5xl sm:leading-none md:text-6xl">
+    class="text-4xl sm:text-5xl md:text-6xl sm:leading-none leading-10 tracking-tight font-extrabold text-thatBlue-800">
     Thank You for your support
   </h2>
   <div class="mt-12">
-    <p class="text-base text-gray-500 sm:text-lg md:mt-5 md:text-xl max-w-5xl">
+    <p
+      class="mt-3 sm:mt-5 md:mt-5 sm:mx-auto lg:mx-0
+      text-base text-gray-500  
+      sm:text-lg md:text-xl
+      sm:max-w-5xl">
       Since the start of THAT, it has been the continuous support from companies
       and organizations partnering with us to help create events that bring the
       community together. We thank each and every one of our partners, across
