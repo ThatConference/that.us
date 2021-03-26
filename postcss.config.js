df545edff6967da5b8705a251633584b94204f1c
Clark Sell <clark.sell@csell.net>
2021-03-26 11:56:41 -0500

    enables tailwindjit

--- postcss.config.js
+++ postcss.config.js
@@ -1,20 +1,9 @@
 /* eslint-disable global-require */
 /* eslint-disable no-undef */
 
-const tailwindcss = require('tailwindcss');
-
-const plugins =
-  process.env.NODE_ENV === 'production'
-    ? [
-        tailwindcss('./tailwind.config.js'),
-        require('@tailwindcss/ui'),
-        require('autoprefixer'),
-        require('@fullhuman/postcss-purgecss'),
-      ]
-    : [
-        tailwindcss('./tailwind.config.js'),
-        require('@tailwindcss/ui'),
-        require('autoprefixer'),
-      ];
+const plugins = {
+  '@tailwindcss/jit': {},
+  autoprefixer: {},
+};
 
 module.exports = { plugins };
