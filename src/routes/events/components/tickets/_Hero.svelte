8a5e2adae6bfe01dc61ee13b63ce4251fe30934e
Clark Sell <clark.sell@csell.net>
2021-04-19 18:24:43 -0500

    updates hero layout and adds sassy

--- src/routes/events/components/tickets/_Hero.svelte
+++ src/routes/events/components/tickets/_Hero.svelte
@@ -9,7 +9,7 @@
 
 <div class="relative bg-thatBlue-200 bg-opacity-25">
   <div class="max-w-5xl mx-auto w-full pt-10 pb-10 px-6">
-    <div class="flex items-center">
+    <div class="flex flex-col sm:flex-row items-center">
       <div class="flex flex-col space-y-4">
         <h1
           class="font-extrabold uppercase text-thatBlue-500 text-4xl sm:text-5xl md:text-6xl lg:text-5xl xl:text-6xl">
@@ -38,8 +38,8 @@
           <p class="text-3xl tracking-tight text-gray-500"></p>
         </div>
       </div>
-      <div class="">
-        <img class="h-96 " src="{event.logo}" alt="" />
+      <div class="mt-12 sm:mt-0">
+        <img class="h-48 sm:h-96 " src="{event.logo}" alt="" />
       </div>
     </div>
   </div>
