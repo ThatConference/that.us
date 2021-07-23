478b7f831a75c7738b8ce323529705f904d3f010
Clark Sell <clark.sell@csell.net>
2021-07-22 21:56:16 -0500

    adds contact exchange

--- src/components/numberPad/PanelButton.svelte
+++ src/components/numberPad/PanelButton.svelte
@@ -7,7 +7,7 @@
 <button
   on:click="{() => dispatch('press')}"
   on:keyPress="{() => dispatch('press')}"
-  class="z-10 border-2 border-gray-200 hover:bg-gray-200 shadow m-2 rounded-full h-24 w-24 flex items-center justify-center text-gray-900 select-none focus:outline-none focus:shadow-outline {$$props.class}"
+  class="z-10 border-2 bg-white border-gray-200 hover:bg-gray-200 shadow m-2 rounded-full h-24 w-24 flex items-center justify-center text-gray-900 select-none focus:outline-none focus:shadow-outline {$$props.class}"
   class:font-bold="{bold}"
   class:text-2xl="{bold}">
   <slot />
