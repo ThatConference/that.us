71814b4871ac3de77bfe501f5b2a570a0e058665
Clark Sell <clark.sell@csell.net>
2021-09-07 09:15:21 -0500

    updates old session route redirection

--- src/routes/session-redirect.svelte
+++
@@ -1,6 +0,0 @@
-<script>
-	import { goto } from '$app/navigation';
-
-	let currentPath = window.location.pathname.replace('sessions', 'activities');
-	goto(`${currentPath}${window.location.search}`);
-</script>
