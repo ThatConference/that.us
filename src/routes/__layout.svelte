94db306b89263b940e07dea88c3c213c50550f47
Clark Sell <clark.sell@csell.net>
2021-09-16 17:24:16 -0500

    adds in our 3rd party integrations

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -1,138 +1,149 @@
 <script context="module">
-</script>
-
-<script>
-	import { browser } from '$app/env';
 	import { v4 as uuidv4 } from 'uuid';
-
-	import { onMount, onDestroy, setContext } from 'svelte';
-	import { navigating } from '$app/stores';
-
+	import { dev } from '$app/env';
+	import { inspect } from '@xstate/inspect';
 	import * as Sentry from '@sentry/browser';
+	import { Integrations } from '@sentry/tracing';
 	import LogRocket from 'logrocket';
 	import lodash from 'lodash';
 
-	import loading from '$stores/loading';
-	import { createAuth } from '$utils/security';
-	import config from '$utils/config';
-	import cart from '$utils/cart';
-	import Preloading from '$components/preloading.svelte';
+	const correlationId = uuidv4();
 
+	if (!dev) {
+		LogRocket.init(logging.logRocket);
+		LogRocket.info('correlationId', correlationId);
+
+		Sentry.init({
+			dsn: logging.dsn,
+			release: config.version,
+			environment: logging.environment,
+			debug: false,
+			attachStacktrace: true,
+			integrations: [new Integrations.BrowserTracing()]
+		});
+
+		LogRocket.getSessionURL((sessionURL) => {
+			Sentry.configureScope((scope) => {
+				scope.setExtra('LogRocketSession', sessionURL);
+			});
+		});
+
+		Sentry.configureScope((scope) => {
+			scope.setTag('correlationId', correlationId);
+		});
+	}
+
+	if (debug.xstate) {
+		inspect({
+			url: 'https://statecharts.io/inspect',
+			iframe: false
+		});
+	}
+
+	export async function load({}) {
+		return {
+			context: {
+				correlationId
+			},
+			props: {
+				correlationId
+			}
+		};
+	}
+</script>
+
+<script>
+	import { onMount, onDestroy, setContext } from 'svelte';
+	import { browser } from '$app/env';
+	import { navigating } from '$app/stores';
+
+	import loading from '$stores/loading';
+	import { showReleaseNotes } from '$stores/siteVersion';
+	import { messages } from '$stores/notificationCenter';
+
+	import { createAuth } from '$utils/security';
+	import cart from '$utils/cart';
+	import config, { debug, logging } from '$utils/config';
+
+	import Preloading from '$components/preloading.svelte';
 	import Tailwindcss from '$elements/Tailwindcss.svelte';
 
-	// import { showReleaseNotes } from '$stores/siteVersion';
-	// import { messages } from '$stores/notificationCenter';
+	// setup the context on the cart for later usage
+	setContext('cart', cart);
+	setContext('correlationId', correlationId);
 
 	const { isEmpty } = lodash;
 	const { isAuthenticated, thatProfile } = createAuth();
+	let unsub;
 
-	setContext('cart', cart);
-
-	// function createCorrelationId() {
-	// 	const id = uuidv4();
-	// 	LogRocket.info('correlationId', id);
-
-	// 	Sentry.configureScope((scope) => {
-	// 		scope.setTag('correlationId', id);
-	// 	});
-
-	// 	return id;
-	// }
-
-	// function onTidioChatApiReady() {
-	// 	/*
-	//     resets the z-index of the tidio iframe such that it's under warnings.
-	//     ... we might have to set it better based on some other layering going on in places.
-	//   */
-	// 	document.getElementById('tidio-chat-iframe').style.zIndex = '40';
-
-	// 	unsub = thatProfile.subscribe((currentUser) => {
-	// 		if (!isEmpty(currentUser)) {
-	// 			window.tidioChatApi.setVisitorData({
-	// 				distinct_id: currentUser.id,
-	// 				email: currentUser.email,
-	// 				name: `${currentUser.firstName} ${currentUser.lastName}`
-	// 			});
-
-	// 			window.tidioChatApi.setContactProperties({
-	// 				company: currentUser.company,
-	// 				canfeature: currentUser.canFeature ? 'true' : 'false'
-	// 			});
-
-	// 			window.tidioChatApi.addVisitorTags([
-	// 				currentUser.id,
-	// 				`https://that.us/member/${currentUser.profileSlug}`
-	// 			]);
-	// 		}
-	// 	});
-	// }
-
-	// let client = initClient({
-	// 	url: config.api,
-	// 	fetchOptions: () => ({
-	// 		headers: {
-	// 			authorization: $token ? `Bearer ${$token}` : '',
-	// 			'that-site': 'that.us'
-	// 			// 'that-correlation-id': createCorrelationId()
-	// 		}
-	// 	}),
-	// 	// todo.. this needs to be revisited... and when we get a new graph client.
-	// 	// requestPolicy: 'cache-and-network',
-	// 	requestPolicy: 'network-only'
-	// });
-
-	// router.subscribe((e) => {
-	// 	if (!e.initial) {
-	// 		window.woopra.track();
-	// 	}
-	// });
+	function onTidioChatApiReady() {
+		/*
+      resets the z-index of the tidio iframe such that it's under warnings.
+      ... we might have to set it better based on some other layering going on in places.
+    */
+		document.getElementById('tidio-chat-iframe').style.zIndex = '40';
+		unsub = thatProfile.subscribe((currentUser) => {
+			if (!isEmpty(currentUser)) {
+				window.tidioChatApi.setVisitorData({
+					distinct_id: currentUser.id,
+					email: currentUser.email,
+					name: `${currentUser.firstName} ${currentUser.lastName}`
+				});
+				window.tidioChatApi.setContactProperties({
+					company: currentUser.company,
+					canfeature: currentUser.canFeature ? 'true' : 'false'
+				});
+				window.tidioChatApi.addVisitorTags([
+					currentUser.id,
+					`https://that.us/member/${currentUser.profileSlug}`
+				]);
+			}
+		});
+	}
 
 	onMount(() => {
-		// if ($showReleaseNotes) {
-		// 	messages.update((m) => [
-		// 		...m,
-		// 		{
-		// 			message: '🙌 We shipped again! 🎉 Check out newest features on THAT.us!!!',
-		// 			url: '/changelog-missed'
-		// 		}
-		// 	]);
-		// }
-		// if (browser) {
-		// 	if (window.tidioChatApi) {
-		// 		window.tidioChatApi.on('ready', onTidioChatApiReady);
-		// 	} else {
-		// 		document.addEventListener('tidioChat-ready', onTidioChatApiReady);
-		// 	}
-		// }
+		if ($showReleaseNotes) {
+			messages.update((m) => [
+				...m,
+				{
+					message: '🙌 We shipped again! 🎉 Check out newest features on THAT.us!!!',
+					url: '/changelog-missed'
+				}
+			]);
+		}
+
+		if (window.tidioChatApi) {
+			window.tidioChatApi.on('ready', onTidioChatApiReady);
+		} else {
+			document.addEventListener('tidioChat-ready', onTidioChatApiReady);
+		}
 	});
 
+	$: if (!isEmpty($thatProfile)) {
+		if (!dev) {
+			let { id, email, firstName, lastName } = $thatProfile;
+
+			Sentry.configureScope((scope) => {
+				scope.setUser({
+					email,
+					id
+				});
+			});
+
+			LogRocket.identify(id, {
+				email
+			});
+
+			woopra.identify({
+				id,
+				email,
+				name: `${firstName} ${lastName}`
+			});
+		}
+	}
+
 	// onDestroy(unsub);
-
-	// $: if (!isEmpty($thatProfile)) {
-	// 	let { id, email, firstName, lastName } = $thatProfile;
-
-	// 	Sentry.configureScope((scope) => {
-	// 		scope.setUser({
-	// 			email,
-	// 			id
-	// 		});
-	// 	});
-
-	// 	LogRocket.identify(id, {
-	// 		email
-	// 	});
-
-	// 	woopra.identify({
-	// 		id,
-	// 		email,
-	// 		name: `${firstName} ${lastName}`
-	// 	});
-	// }
 </script>
-
-<svelte:head>
-	<script src="https://js.stripe.com/v3" async></script>
-</svelte:head>
 
 <div>
 	{#if $navigating || $loading}
@@ -172,7 +183,9 @@
 		white-space: pre-line;
 	}
 
-	/* * {
-    outline: 1px solid red;
-  } */
+	/* for debuging purposes when we want to punch a wall.
+		* {
+    	outline: 1px solid red;
+		} 
+	*/
 </style>
