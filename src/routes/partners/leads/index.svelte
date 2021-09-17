0008b7ae8fa507ffb158d77dea9c6c09bf114087
Clark Sell <clark.sell@csell.net>
2021-09-17 13:38:20 -0500

    adds secure loading to partner and event pages

---
+++ src/routes/partners/leads/index.svelte
@@ -0,0 +1,10 @@
+<script context="module">
+	export async function load() {
+		const eventId = '7wiuRWI7EZjcdF4e9MDz';
+
+		return {
+			status: 301,
+			redirect: `/partners/leads/${eventId}`
+		};
+	}
+</script>
