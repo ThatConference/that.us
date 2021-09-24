436f2ba391c32d3c038c2790dbb55e4c1a793cf4
Clark Sell <clark.sell@csell.net>
2021-09-24 14:48:12 -0500

    debug debug debug

--- src/routes/login.js
+++ src/routes/login.js
@@ -1,6 +1,7 @@
 import auth0 from '$utils/security';
 
 export function get(svelteReq) {
+	console.log('login api was called');
 	const returnTo = svelteReq.query.get('returnTo');
 	return auth0.handleLogin(svelteReq, { returnTo });
 }
