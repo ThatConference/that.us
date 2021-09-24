9cd0226c7abbd3062385afce6d829cade11b73d1
Clark Sell <clark.sell@csell.net>
2021-09-24 09:03:47 -0500

    adding logging around auth handler for debugging

--- src/routes/api/auth/callback.js
+++ src/routes/api/auth/callback.js
@@ -1,13 +1,7 @@
 import auth0 from '$utils/security';
-import * as Sentry from '@sentry/node';
-import { logging } from '$utils/config';
 import fetch from 'isomorphic-fetch';
 
 import { QUERY_ME } from '$dataSources/api.that.tech/me';
-
-Sentry.init({
-	dsn: logging.dsn
-});
 
 const endpoint = `https://api.that.tech/graphql/`;
 
@@ -20,37 +14,29 @@ async function afterCallback(req, res, session, state) {
 	};
 
 	try {
-		console.log('in afterCallback');
 		const results = await fetch(endpoint, {
 			method: 'POST',
 			headers: {
+				credentials: 'include',
 				'Content-Type': 'application/json',
 				Authorization: `Bearer ${session.accessToken}`
 			},
 			body: JSON.stringify(body)
-		})
-			.then((r) => r.json())
-			.catch((error) => {
-				console.error('ERROR After Callback Fetching THAT Profile', error);
-			});
+		}).then((r) => r.json());
 
 		session.thatProfile = results.data.members?.me;
 		return session;
 	} catch (error) {
 		console.error('Fetch Call Error', error);
-		Sentry.captureException(error, { req });
-
 		return session;
 	}
 }
 
 export function get(req, res) {
-	console.log('handle callback req', req);
 	try {
 		return auth0.handleCallback(req, { afterCallback });
 	} catch (error) {
 		console.error('error in handle callback', error);
-		Sentry.captureException(error, { req });
 		res.status(error.status || 400).end(error.message);
 	}
 }
