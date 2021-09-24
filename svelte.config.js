8e42d172796beceeb090bb05ceaf5a5cf763903d
Clark Sell <clark.sell@csell.net>
2021-09-24 13:46:02 -0500

    trailing slash

--- svelte.config.js
+++ svelte.config.js
@@ -11,7 +11,7 @@ const config = {
 	kit: {
 		// hydrate the <div id="svelte"> element in src/app.html
 		target: '#svelte',
-		trailingSlash: 'always',
+		trailingSlash: 'ignore',
 		vite: {
 			resolve: {
 				alias: {
