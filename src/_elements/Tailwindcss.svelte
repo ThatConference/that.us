4b2e58108058266946d10366b7473dc59f6194f8
Clark Sell <clark.sell@csell.net>
2021-10-29 09:08:20 -0500

    moving back to tailwinds global

--- src/_elements/Tailwindcss.svelte
+++ src/_elements/Tailwindcss.svelte
@@ -2,4 +2,17 @@
 	@import 'tailwindcss/base';
 	@import 'tailwindcss/components';
 	@import 'tailwindcss/utilities';
+
+	:global(.strike) {
+		text-decoration: line-through;
+	}
+
+	:global(.prose a) {
+		text-decoration: none;
+		color: #26529a;
+	}
+
+	:global(.prose a:hover) {
+		color: #ffffff;
+	}
 </style>
