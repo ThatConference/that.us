7bd8c4d6f233a8511f974e7a68e97253e7836e4c
Clark Sell <clark.sell@csell.net>
2021-12-02 19:56:29 -0600

    updates how we pull og-image for activites

---
+++ src/routes/api/og-image/[id].js
@@ -0,0 +1,27 @@
+import fetch from 'isomorphic-fetch';
+import config from '$utils/config';
+
+const baseHostImage = 'https://that.imgix.net/og-image';
+
+export async function get({ params }) {
+	let { id } = params;
+	const response = await fetch(`${baseHostImage}/${id}.png`);
+
+	let body;
+
+	if (response.status === 200) {
+		const buffer = await response.arrayBuffer();
+		body = new Uint8Array(buffer);
+	} else {
+		const r = await fetch(`${config.hostURL}/images/ActivityDefault.jpg`);
+		const imageBuffer = await r.arrayBuffer();
+		body = new Uint8Array(imageBuffer);
+	}
+
+	return {
+		headers: {
+			'Content-Type': 'image/png'
+		},
+		body
+	};
+}
