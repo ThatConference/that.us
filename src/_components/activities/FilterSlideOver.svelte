22d678576443eac860077678fb1464b37757c338
Adam J Tegen <36305171+ategen3rt@users.noreply.github.com>
2021-12-10 15:45:43 -0600

    Added filtering for onThat, atThat, family, open spaces, and workshops

--- src/_components/activities/FilterSlideOver.svelte
+++ src/_components/activities/FilterSlideOver.svelte
@@ -4,6 +4,11 @@
 	export let communities;
 	export let events = [];
 	export let selectedFilterTerms = [];
+	export let onThat;
+	export let atThat;
+	export let family;
+	export let openSpace;
+	export let workshop;
 
 	import Icon from 'svelte-awesome';
 	import { filter as filterIcon, externalLink } from '$components/svelte-awesome-icons';
@@ -25,7 +30,7 @@
 	</div>
 
 	<div class="flex flex-col divide-y divide-gray-400">
-		<div class="py-12">
+		<div class="pb-4">
 			<h2 class="text-lg leading-7 font-medium text-gray-900">Text Search</h2>
 
 			<div class="my-2 border-gray-200">
@@ -38,9 +43,8 @@
 		</div>
 
 		{#if eventsModified.length > 0}
-			<div class="py-12">
+			<div class="pt-4 pb-2">
 				<h2 class="text-lg leading-7 font-medium text-gray-900">Events</h2>
-
 				<div class="my-2 border-gray-200">
 					<ul>
 						{#each eventsModified as event}
@@ -59,38 +63,80 @@
 				</div>
 			</div>
 		{/if}
-
-		<div class="px-4 py-12 grid grid-cols-2 gap-6">
-			<div>
-				<h2 class="text-lg leading-7 font-medium text-gray-900">Tags</h2>
+		<div class="py-4">
+			<h2 class="text-lg leading-7 font-medium text-gray-900">Location</h2>
+			<div class="border-gray-200">
 				<fieldset class="flex flex-col">
-					{#each tags as tag}
-						{#if tag.charAt(0) !== '@'}
-							<label
-								class="capitalize text-base text-gray-500 sm:text-lg md:mt-2 md:text-md lg:mx-0 whitespace-nowrap"
-							>
-								<input type="checkbox" bind:group={selectedFilterTerms} value={tag} />
-								<span class="px-2">{tag}</span>
-							</label>
-						{/if}
+					<label
+						class="capitalize text-base text-gray-500 sm:text-lg md:mt-1 md:text-md lg:mx-0 whitespace-nowrap"
+					>
+						<input type="checkbox" bind:checked={atThat} />
+						<span class="px-2">AT THAT</span>
+					</label>
+					<label
+						class="capitalize text-base text-gray-500 sm:text-lg md:mt-1 md:text-md lg:mx-0 whitespace-nowrap"
+					>
+						<input type="checkbox" bind:checked={onThat} />
+						<span class="px-2">ON THAT</span>
+					</label>
+				</fieldset>
+			</div>
+		</div>
+		<div class="py-4">
+			<h2 class="text-lg leading-7 font-medium text-gray-900">Category</h2>
+			<div class="border-gray-200">
+				<fieldset class="flex flex-col">
+					<label
+						class="capitalize text-base text-gray-500 sm:text-lg md:mt-1 md:text-md lg:mx-0 whitespace-nowrap"
+					>
+						<input type="checkbox" bind:checked={family} />
+						<span class="px-2">Family</span>
+					</label>
+					<label
+						class="capitalize text-base text-gray-500 sm:text-lg md:mt-1 md:text-md lg:mx-0 whitespace-nowrap"
+					>
+						<input type="checkbox" bind:checked={openSpace} />
+						<span class="px-2">Open Space</span>
+					</label>
+					<label
+						class="capitalize text-base text-gray-500 sm:text-lg md:mt-1 md:text-md lg:mx-0 whitespace-nowrap"
+					>
+						<input type="checkbox" bind:checked={workshop} />
+						<span class="px-2">Workshop</span>
+					</label>
+				</fieldset>
+			</div>
+		</div>
+
+		<div class="py-4">
+			<h2 class="text-lg leading-7 font-medium text-gray-900">Communities</h2>
+			<div class="mb-2">
+				<fieldset class="flex flex-col">
+					{#each communities as community}
+						<label
+							class="capitalize text-base text-gray-500 sm:text-lg md:mt-2 md:text-md lg:mx-0 whitespace-nowrap"
+						>
+							<input type="checkbox" bind:group={selectedFilterTerms} value="@{community}" />
+							<span class="px-2">@{community}</span>
+						</label>
 					{/each}
 				</fieldset>
 			</div>
-			<div>
-				<h2 class="text-lg leading-7 font-medium text-gray-900">Communities</h2>
-				<div class="mb-2">
-					<fieldset class="flex flex-col">
-						{#each communities as community}
-							<label
-								class="capitalize text-base text-gray-500 sm:text-lg md:mt-2 md:text-md lg:mx-0 whitespace-nowrap"
-							>
-								<input type="checkbox" bind:group={selectedFilterTerms} value="@{community}" />
-								<span class="px-2">@{community}</span>
-							</label>
-						{/each}
-					</fieldset>
-				</div>
-			</div>
+		</div>
+		<div class="py-4">
+			<h2 class="text-lg leading-7 font-medium text-gray-900">Tags</h2>
+			<fieldset class="flex flex-col">
+				{#each tags as tag}
+					{#if tag.charAt(0) !== '@'}
+						<label
+							class="capitalize text-base text-gray-500 sm:text-lg md:mt-2 md:text-md lg:mx-0 whitespace-nowrap"
+						>
+							<input type="checkbox" bind:group={selectedFilterTerms} value={tag} />
+							<span class="px-2">{tag}</span>
+						</label>
+					{/if}
+				{/each}
+			</fieldset>
 		</div>
 	</div>
 	<div slot="panelActions">
