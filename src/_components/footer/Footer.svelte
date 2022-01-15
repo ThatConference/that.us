8a3f3f94d3fd887eced9a8336c535a3067062a40
Clark Sell <clark.sell@csell.net>
2022-01-15 09:01:39 -0600

    feat: updates layout and adds newsletter

--- src/_components/footer/Footer.svelte
+++ src/_components/footer/Footer.svelte
@@ -3,6 +3,7 @@
 
 	import { SocialLink } from '../social';
 	import socials from './socials';
+	import Newsletter from './Newsletter.svelte';
 
 	import {
 		thatLinks,
@@ -30,121 +31,119 @@
 					{/each}
 				</div>
 			</div>
-			<div class="mt-12 grid grid-cols-2 gap-8 xl:mt-0 md:col-span-2">
-				<div>newsletter signup</div>
+			<div class="mt-12 col-span-2 flex justify-end  xl:mt-0 ">
+				<Newsletter />
 			</div>
 		</div>
 
-		<div class="col-span-3 mt-12 pt-12 grid grid-cols-3 gap-8 xl:mt-0">
-			<div class="md:grid md:grid-cols-2 md:gap-2">
-				<div>
-					<h3 class="text-sm font-semibold text-gray-400 tracking-wider uppercase">THAT</h3>
-					<ul class="mt-6 space-y-4">
-						{#each thatLinks as thatLink}
-							<li>
-								<a
-									href={thatLink.href}
-									class="text-base text-gray-500 hover:text-that-blue focus:border-thatOrange-700 focus:ring-that-orange"
-								>
-									{thatLink.text}
-								</a>
-							</li>
-						{/each}
-					</ul>
-				</div>
-
-				<div class="mt-12 md:mt-0">
-					<h3 class="text-sm font-semibold text-gray-400 tracking-wider uppercase">
-						THAT Conference
-					</h3>
-					<ul class="mt-6 space-y-4">
-						{#each thatConferenceLinks as link}
-							<li>
-								<a
-									href={link.href}
-									class="text-base text-gray-500 hover:text-that-blue focus:border-thatOrange-700 focus:ring-that-orange"
-								>
-									{link.text}
-								</a>
-							</li>
-						{/each}
-					</ul>
-				</div>
+		<div
+			class="xl:col-span-3 grid grid-cols-2 md:grid-cols-3 xl:grid-cols-6 gap-12 mt-12 pt-12 xl:mt-0"
+		>
+			<div>
+				<h3 class="text-sm font-semibold text-gray-400 tracking-wider uppercase">THAT</h3>
+				<ul class="mt-6 space-y-4">
+					{#each thatLinks as thatLink}
+						<li>
+							<a
+								href={thatLink.href}
+								class="text-base text-gray-500 hover:text-that-blue hover:font-semibold focus:border-thatOrange-700 focus:ring-that-orange"
+							>
+								{thatLink.text}
+							</a>
+						</li>
+					{/each}
+				</ul>
 			</div>
 
-			<div class="md:grid md:grid-cols-2 md:gap-8">
-				<div>
-					<h3 class="text-sm font-semibold text-gray-400 tracking-wider uppercase">Support</h3>
-					<ul class="mt-6 space-y-4">
-						{#each supportLinks as supportLink}
-							<li>
-								<a
-									href={supportLink.href}
-									class="text-base text-gray-500 hover:text-that-blue focus:border-thatOrange-700 focus:ring-that-orange"
-								>
-									{supportLink.text}
-								</a>
-							</li>
-						{/each}
-					</ul>
-				</div>
-
-				<div class="mt-12 md:mt-0">
-					<h3 class="text-sm font-semibold text-gray-400 tracking-wider uppercase">Company</h3>
-					<ul class="mt-6 space-y-4">
-						{#each companyLinks as companyLink}
-							<li>
-								<a
-									href={companyLink.href}
-									class="text-base text-gray-500 hover:text-that-blue focus:border-thatOrange-700 focus:ring-that-orange"
-								>
-									{companyLink.text}
-								</a>
-							</li>
-						{/each}
-					</ul>
-				</div>
+			<div>
+				<h3 class="text-sm font-semibold text-gray-400 tracking-wider uppercase">
+					THAT Conference
+				</h3>
+				<ul class="mt-6 space-y-4">
+					{#each thatConferenceLinks as link}
+						<li>
+							<a
+								href={link.href}
+								class="text-base text-gray-500 hover:text-that-blue hover:font-semibold focus:border-thatOrange-700 focus:ring-that-orange"
+							>
+								{link.text}
+							</a>
+						</li>
+					{/each}
+				</ul>
 			</div>
 
-			<div class="md:grid md:grid-cols-2 md:gap-8">
-				<div>
-					<h3 class="text-sm font-semibold text-gray-400 tracking-wider uppercase">Legal</h3>
-					<ul class="mt-6 space-y-4">
-						{#each legalLinks as leaglLink}
-							<li>
-								<a
-									href={leaglLink.href}
-									class="text-base text-gray-500 hover:text-that-blue focus:border-thatOrange-700 focus:ring-that-orange"
-								>
-									{leaglLink.text}
-								</a>
-							</li>
-						{/each}
-					</ul>
-				</div>
+			<div>
+				<h3 class="text-sm font-semibold text-gray-400 tracking-wider uppercase">Support</h3>
+				<ul class="mt-6 space-y-4">
+					{#each supportLinks as supportLink}
+						<li>
+							<a
+								href={supportLink.href}
+								class="text-base text-gray-500 hover:text-that-blue hover:font-semibold focus:border-thatOrange-700 focus:ring-that-orange"
+							>
+								{supportLink.text}
+							</a>
+						</li>
+					{/each}
+				</ul>
+			</div>
 
-				<div class="mt-12 md:mt-0">
-					<h3 class="text-sm font-semibold text-gray-400 tracking-wider uppercase">Account</h3>
-					<ul class="mt-6 space-y-4">
-						{#each myProfileLinks as link}
-							<li>
-								<a
-									href={link.href}
-									class="text-base text-gray-500 hover:text-that-blue focus:border-thatOrange-700 focus:ring-that-orange"
-								>
-									{link.text}
-								</a>
-							</li>
-						{/each}
-					</ul>
-				</div>
+			<div>
+				<h3 class="text-sm font-semibold text-gray-400 tracking-wider uppercase">Company</h3>
+				<ul class="mt-6 space-y-4">
+					{#each companyLinks as companyLink}
+						<li>
+							<a
+								href={companyLink.href}
+								class="text-base text-gray-500 hover:text-that-blue hover:font-semibold focus:border-thatOrange-700 focus:ring-that-orange"
+							>
+								{companyLink.text}
+							</a>
+						</li>
+					{/each}
+				</ul>
+			</div>
+
+			<div>
+				<h3 class="text-sm font-semibold text-gray-400 tracking-wider uppercase">Legal</h3>
+				<ul class="mt-6 space-y-4">
+					{#each legalLinks as leaglLink}
+						<li>
+							<a
+								href={leaglLink.href}
+								class="text-base text-gray-500 hover:text-that-blue hover:font-semibold focus:border-thatOrange-700 focus:ring-that-orange"
+							>
+								{leaglLink.text}
+							</a>
+						</li>
+					{/each}
+				</ul>
+			</div>
+
+			<div>
+				<h3 class="text-sm font-semibold text-gray-400 tracking-wider uppercase">Account</h3>
+				<ul class="mt-6 space-y-4">
+					{#each myProfileLinks as link}
+						<li>
+							<a
+								href={link.href}
+								class="text-base text-gray-500 hover:text-that-blue hover:font-semibold focus:border-thatOrange-700 focus:ring-that-orange"
+							>
+								{link.text}
+							</a>
+						</li>
+					{/each}
+				</ul>
 			</div>
 		</div>
 
-		<div class="mt-12 border-t border-gray-200 pt-8">
-			<p class="text-base text-gray-400 xl:text-center">
-				&copy; {dayjs().format('YYYY')} THAT&reg; All rights reserved.
-			</p>
+		<div>
+			<div class="mt-12 border-t border-gray-200 pt-8">
+				<p class="text-base text-gray-400 xl:text-center">
+					&copy; {dayjs().format('YYYY')} THAT&reg; All rights reserved.
+				</p>
+			</div>
 		</div>
 	</div>
 </footer>
