bbd3990357d794218613027ca2874a4058507d9d
Clark Sell <clark.sell@csell.net>
2022-04-09 08:08:34 -0500

    fix: fixes some error flow on tickets

--- src/routes/speakers/_components/formSections/SelectTicketDetails.svelte
+++ src/routes/speakers/_components/formSections/SelectTicketDetails.svelte
@@ -13,8 +13,8 @@
 	const isAt = platform === 'AT_THAT';
 	const ticketResults = [];
 
-	let canContinue = false;
-	let ticketError = false;
+	$: canContinue = false;
+	$: ticketError = false;
 
 	$: allocatedAllocations = orderAllocations.filter((oa) => oa.isAllocated);
 
@@ -22,7 +22,7 @@
 		ticketResults.push(detail.wasCreated);
 		canContinue = false;
 
-		if (orderAllocations.length === ticketResults.length) {
+		if (orderAllocations.length - 1 === ticketResults.length) {
 			ticketError = ticketResults.includes(false);
 			canContinue = !ticketError;
 		}
@@ -38,32 +38,50 @@
 	</p>
 </div>
 
-<div class="mt-12">
-	{#if allocatedAllocations.length === 0}
-		<div class="flex flex-1 items-center justify-between rounded-md border bg-gray-50 p-4">
-			<h2 class="text-xl font-extrabold text-gray-800">Fetching Your Tickets...</h2>
-			<Busy />
-		</div>
-	{:else}
-		<h2 class="text-xl text-gray-800 font-extrabold">Please complete each ticket.</h2>
-		{#each allocatedAllocations as orderAllocation, i}
-			<TicketDetail
-				{orderId}
-				{orderAllocation}
-				isOpen={i === 0}
-				on:ticket-created={handleTicketCreated} />
-		{/each}
-	{/if}
-</div>
-
-<div class="mt-12">
-	{#if ticketError}
-		<p>Your ticket is in error. contact us.</p>
-	{:else}
-		<div class="flex justify-end">
-			<button disabled={canContinue} on:click={() => dispatch('submit-step')}>
+{#if ticketError}
+	<div class="mt-12">
+		<h2 class="text-xl font-extrabold text-red-500">Well we sure didn't expect that.</h2>
+		<p class="prose prose-lg mt-4 text-gray-500">
+			The robots have failed us. There is an issue trying to complete your ticket. Please contact us
+			so we can sort this out with you.
+		</p>
+		<div class="mt-4 flex">
+			<a
+				href={`mailto:hello@that.us?subject=Error Creating My Speaker Tickets&body=The robots have failed us. Order Id ${orderId}`}
+				rel="noopener"
+				target="_blank">
 				<Shell>
-					<div class="px-8 py-2 font-extrabold" class:bg:grey-400={!canContinue}>
+					<div class="px-8 py-2 font-extrabold">Contact Us</div>
+				</Shell>
+			</a>
+		</div>
+	</div>
+{:else}
+	<div class="mt-12">
+		{#if allocatedAllocations.length === 0}
+			<div class="flex flex-1 items-center justify-between rounded-md border bg-gray-50 p-4">
+				<h2 class="text-xl font-extrabold text-gray-800">Fetching Your Tickets...</h2>
+				<Busy />
+			</div>
+		{:else}
+			<h2 class="text-xl text-gray-800 font-extrabold">Please complete each ticket.</h2>
+			{#each allocatedAllocations as orderAllocation, i}
+				<TicketDetail
+					{orderId}
+					{orderAllocation}
+					isOpen={i === 0}
+					on:ticket-created={handleTicketCreated} />
+			{/each}
+		{/if}
+	</div>
+{/if}
+
+{#if canContinue}
+	<div class="mt-12">
+		<div class="flex justify-end">
+			<button on:click={() => dispatch('submit-step')}>
+				<Shell>
+					<div class="px-8 py-2 font-extrabold">
 						{#if isAt}
 							Next, Emergency Contact
 						{:else}
@@ -73,5 +91,5 @@
 				</Shell>
 			</button>
 		</div>
-	{/if}
-</div>
+	</div>
+{/if}
