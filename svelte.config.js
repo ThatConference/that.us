336256aae15176484bc7f59ca669c4963a413877
Clark Sell <clark.sell@csell.net>
2022-05-18 17:55:19 -0500

    chore: adding vercel adapter back

--- svelte.config.js
+++ svelte.config.js
@@ -1,4 +1,5 @@
-import adapter from '@sveltejs/adapter-auto';
+// import adapter from '@sveltejs/adapter-auto';
+import adapter from '@sveltejs/adapter-vercel';
 import preprocess from 'svelte-preprocess';
 import { resolve } from 'path';
 import { mdsvex } from 'mdsvex';
