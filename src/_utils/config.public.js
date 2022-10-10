60c6e753a08e3363e9e9bd43bee7c5fe893cbf35
Clark Sell <clark.sell@csell.net>
2022-10-10 16:36:37 -0500

    chore: updates version number

--- src/_utils/config.public.js
+++ src/_utils/config.public.js
@@ -8,7 +8,7 @@ function configMissing(configKey) {
 export default {
 	hostURL: env.PUBLIC_HOST_URL || 'https://that.us',
 	nodeEnv: env.NODE_ENV,
-	version: '3.0.1',
+	version: '3.0.2',
 	eventId: 'YWavA70szR8rxSwrLJaL',
 	eventSlug: 'thatus/daily',
 	api: {
--- src/routes/+layout.js
+++ src/routes/+layout.js
@@ -22,8 +22,8 @@ if (!dev) {
 
 	Sentry.init({
 		dsn: logging.dsn,
-		dist: 'that-us:3.0.1',
-		release: 'that-us:3.0.1',
+		dist: 'that-us:3.0.2',
+		release: 'that-us:3.0.2',
 		environment: logging.environment,
 		debug: false,
 		attachStacktrace: true,
