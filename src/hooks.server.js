7ba94d845e65caa87c1de9c6e3b1e10856559212
Clark Sell <clark.sell@csell.net>
2022-10-10 15:15:14 -0500

    fix: events/slug/tickets

--- src/hooks.server.js
+++ src/hooks.server.js
@@ -1,10 +1,10 @@
-// import * as Sentry from '@sentry/svelte';
+import * as Sentry from '@sentry/svelte';
 
-// export async function handleError({ error, event }) {
-// 	Sentry.captureException(error, { event });
+export async function handleError({ error, event }) {
+	Sentry.captureException(error, { event });
 
-// 	return {
-// 		message: 'Whoops!',
-// 		code: error.code ?? 'UNKNOWN'
-// 	};
-// }
+	return {
+		message: 'Whoops!',
+		code: error.code ?? 'UNKNOWN'
+	};
+}
