ddd44f81944e76e080b061692e20e9d98d85e334
Clark Sell <clark.sell@csell.net>
2023-01-16 11:47:40 -0600

    fix: sortby import

--- src/routes/partners/my-network/+layout.js
+++ src/routes/partners/my-network/+layout.js
@@ -1,11 +1,12 @@
 import auth0 from '$utils/security/client';
-import { sortBy } from 'lodash';
+import lodash from 'lodash';
 
 import partnerNetworkApi from '$dataSources/api.that.tech/partner/leads/queries';
 
+const { sortBy } = lodash;
+
 export const load = auth0.withPageAuthRequired({
 	load: async function load({ fetch }) {
-		console.log('in load');
 		const { queryMyNetwork } = partnerNetworkApi(fetch);
 
 		const contacts = await queryMyNetwork()
