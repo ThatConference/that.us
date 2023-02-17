5efc81c2594a03a89adc72be66a43ef35f632a8c
Brett Slaski <brett@thatconference.com>
2023-02-17 09:06:42 -0600

    fix: sourcemaps now uploaded to sentry on build

--- src/_utils/config.public.js
+++ src/_utils/config.public.js
@@ -8,7 +8,7 @@ function configMissing(configKey) {
 export default {
 	hostURL: env.PUBLIC_HOST_URL || 'https://that.us',
 	nodeEnv: env.NODE_ENV,
-	version: '3.1.1',
+	version: '3.2.1',
 	eventId: 'YWavA70szR8rxSwrLJaL',
 	eventSlug: 'thatus/daily',
 	api: {
