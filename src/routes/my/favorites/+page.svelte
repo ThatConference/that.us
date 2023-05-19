919cbde7de69f900bf1831376c7963d9694d36c5
Brett Slaski <brett@thatconference.com>
2023-05-19 14:48:56 -0500

    fix: my favorites sorting and filtering
    feat: add my favorates iCal feed url. This needs syling

--- src/routes/my/favorites/+page.svelte
+++ src/routes/my/favorites/+page.svelte
@@ -1,6 +1,8 @@
 <script>
 	export let data;
 
+	import Icon from 'svelte-awesome';
+	import { copyRegular } from '$components/svelte-awesome-icons';
 	import seoMetaTags from '$utils/seo/metaTags';
 	import Seo from '$components/Seo.svelte';
 	import { ModalWarning, ActionHeader } from '$elements';
@@ -10,8 +12,12 @@
 	import Nav from '$components/nav/interiorNav/Top.svelte';
 	import Sponsor from '$components/SponsorSimple.svelte';
 	import ActivityList from '$components/activities/List.svelte';
+	import favoritesMutationsApi from '$dataSources/api.that.tech/me/favorites/mutations';
 
-	let { activities } = data;
+	let { activities, profile } = data;
+	let iCalUrl = profile.favoritesICalendarUrl;
+	let { rotateICalendarUrl } = favoritesMutationsApi();
+
 	const metaTags = ((title = 'My Favorites - THAT') => ({
 		title,
 		tags: seoMetaTags({
@@ -38,6 +44,30 @@
 	</div>
 
 	<div slot="body">
+		<div>
+			<div class="overflow-x-auto">
+				<h2 class="font-bold leading-9 text-thatBlue-800 sm:text-xl md:text-2xl">
+					Favorites ICAL Feed
+				</h2>
+				<span class="w-8 overflow-hidden text-gray-500">{iCalUrl}</span>
+				<button
+					class="px-1 text-gray-600 hover:text-thatOrange-400"
+					title="copy to clipboard"
+					on:click={async () => await navigator.clipboard.writeText(iCalUrl)}>
+					<Icon data={copyRegular} label="copy to clipboard" />
+				</button>
+			</div>
+			<div>
+				<button
+					type="button"
+					class="mt-2 block text-xs text-gray-500 hover:text-thatOrange-400"
+					on:click={async () => {
+						iCalUrl = await rotateICalendarUrl();
+					}}>
+					Click me to rotate url
+				</button>
+			</div>
+		</div>
 		{#if activities.length > 0}
 			<ActivityList reverse={true} {activities} />
 		{:else}
