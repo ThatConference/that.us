894af00f412d77f3dfd27920088152880bb64378
Brett Slaski <brett@thatconference.com>
2023-06-19 12:18:59 -0500

    feat: add base hubspot form for newletter subs

--- src/_components/footer/Footer.svelte
+++ src/_components/footer/Footer.svelte
@@ -4,6 +4,7 @@
 	import { SocialLink } from '../social';
 	import socials from './socials';
 	import Newsletter from '../newsletter/Footer.svelte';
+	import News2 from '../newsletter/_HSForm.svelte';
 	import { version } from '$app/environment';
 
 	import {
@@ -41,109 +42,112 @@
 			<div class="col-span-2 mt-12 flex justify-end  xl:mt-0 ">
 				<Newsletter />
 			</div>
-		</div>
+			<div <div class="col-span-2 mt-12 flex justify-end  xl:mt-0 ">
+				<News2 />
+			</div>
 
-		<div
-			class="mt-12 grid grid-cols-2 gap-12 pt-12 md:grid-cols-3 xl:col-span-3 xl:mt-0 xl:grid-cols-6">
-			<div>
-				<h3 class="text-sm font-semibold uppercase tracking-wider text-gray-400">THAT</h3>
-				<ul class="mt-6 space-y-4">
-					{#each thatLinks as thatLink}
-						<li>
-							<a
-								href={thatLink.href}
-								class="text-base text-gray-500 hover:font-semibold hover:text-that-blue focus:border-thatOrange-700 focus:ring-that-orange">
-								{thatLink.text}
-							</a>
-						</li>
-					{/each}
-				</ul>
+			<div
+				class="mt-12 grid grid-cols-2 gap-12 pt-12 md:grid-cols-3 xl:col-span-3 xl:mt-0 xl:grid-cols-6">
+				<div>
+					<h3 class="text-sm font-semibold uppercase tracking-wider text-gray-400">THAT</h3>
+					<ul class="mt-6 space-y-4">
+						{#each thatLinks as thatLink}
+							<li>
+								<a
+									href={thatLink.href}
+									class="text-base text-gray-500 hover:font-semibold hover:text-that-blue focus:border-thatOrange-700 focus:ring-that-orange">
+									{thatLink.text}
+								</a>
+							</li>
+						{/each}
+					</ul>
+				</div>
+
+				<div>
+					<h3 class="text-sm font-semibold uppercase tracking-wider text-gray-400">
+						THAT Conference
+					</h3>
+					<ul class="mt-6 space-y-4">
+						{#each thatConferenceLinks as link}
+							<li>
+								<a
+									href={link.href}
+									class="text-base text-gray-500 hover:font-semibold hover:text-that-blue focus:border-thatOrange-700 focus:ring-that-orange">
+									{link.text}
+								</a>
+							</li>
+						{/each}
+					</ul>
+				</div>
+
+				<div>
+					<h3 class="text-sm font-semibold uppercase tracking-wider text-gray-400">Support</h3>
+					<ul class="mt-6 space-y-4">
+						{#each supportLinks as supportLink}
+							<li>
+								<a
+									href={supportLink.href}
+									class="text-base text-gray-500 hover:font-semibold hover:text-that-blue focus:border-thatOrange-700 focus:ring-that-orange">
+									{supportLink.text}
+								</a>
+							</li>
+						{/each}
+					</ul>
+				</div>
+
+				<div>
+					<h3 class="text-sm font-semibold uppercase tracking-wider text-gray-400">Company</h3>
+					<ul class="mt-6 space-y-4">
+						{#each companyLinks as companyLink}
+							<li>
+								<a
+									href={companyLink.href}
+									class="text-base text-gray-500 hover:font-semibold hover:text-that-blue focus:border-thatOrange-700 focus:ring-that-orange">
+									{companyLink.text}
+								</a>
+							</li>
+						{/each}
+					</ul>
+				</div>
+
+				<div>
+					<h3 class="text-sm font-semibold uppercase tracking-wider text-gray-400">Legal</h3>
+					<ul class="mt-6 space-y-4">
+						{#each legalLinks as leaglLink}
+							<li>
+								<a
+									href={leaglLink.href}
+									class="text-base text-gray-500 hover:font-semibold hover:text-that-blue focus:border-thatOrange-700 focus:ring-that-orange">
+									{leaglLink.text}
+								</a>
+							</li>
+						{/each}
+					</ul>
+				</div>
+
+				<div>
+					<h3 class="text-sm font-semibold uppercase tracking-wider text-gray-400">Account</h3>
+					<ul class="mt-6 space-y-4">
+						{#each myProfileLinks as link}
+							<li>
+								<a
+									href={link.href}
+									class="text-base text-gray-500 hover:font-semibold hover:text-that-blue focus:border-thatOrange-700 focus:ring-that-orange">
+									{link.text}
+								</a>
+							</li>
+						{/each}
+					</ul>
+				</div>
 			</div>
 
 			<div>
-				<h3 class="text-sm font-semibold uppercase tracking-wider text-gray-400">
-					THAT Conference
-				</h3>
-				<ul class="mt-6 space-y-4">
-					{#each thatConferenceLinks as link}
-						<li>
-							<a
-								href={link.href}
-								class="text-base text-gray-500 hover:font-semibold hover:text-that-blue focus:border-thatOrange-700 focus:ring-that-orange">
-								{link.text}
-							</a>
-						</li>
-					{/each}
-				</ul>
-			</div>
-
-			<div>
-				<h3 class="text-sm font-semibold uppercase tracking-wider text-gray-400">Support</h3>
-				<ul class="mt-6 space-y-4">
-					{#each supportLinks as supportLink}
-						<li>
-							<a
-								href={supportLink.href}
-								class="text-base text-gray-500 hover:font-semibold hover:text-that-blue focus:border-thatOrange-700 focus:ring-that-orange">
-								{supportLink.text}
-							</a>
-						</li>
-					{/each}
-				</ul>
-			</div>
-
-			<div>
-				<h3 class="text-sm font-semibold uppercase tracking-wider text-gray-400">Company</h3>
-				<ul class="mt-6 space-y-4">
-					{#each companyLinks as companyLink}
-						<li>
-							<a
-								href={companyLink.href}
-								class="text-base text-gray-500 hover:font-semibold hover:text-that-blue focus:border-thatOrange-700 focus:ring-that-orange">
-								{companyLink.text}
-							</a>
-						</li>
-					{/each}
-				</ul>
-			</div>
-
-			<div>
-				<h3 class="text-sm font-semibold uppercase tracking-wider text-gray-400">Legal</h3>
-				<ul class="mt-6 space-y-4">
-					{#each legalLinks as leaglLink}
-						<li>
-							<a
-								href={leaglLink.href}
-								class="text-base text-gray-500 hover:font-semibold hover:text-that-blue focus:border-thatOrange-700 focus:ring-that-orange">
-								{leaglLink.text}
-							</a>
-						</li>
-					{/each}
-				</ul>
-			</div>
-
-			<div>
-				<h3 class="text-sm font-semibold uppercase tracking-wider text-gray-400">Account</h3>
-				<ul class="mt-6 space-y-4">
-					{#each myProfileLinks as link}
-						<li>
-							<a
-								href={link.href}
-								class="text-base text-gray-500 hover:font-semibold hover:text-that-blue focus:border-thatOrange-700 focus:ring-that-orange">
-								{link.text}
-							</a>
-						</li>
-					{/each}
-				</ul>
-			</div>
-		</div>
-
-		<div>
-			<div class="mt-12 border-t border-gray-200 pt-8">
-				<p class="text-base text-gray-400 xl:text-center">
-					&copy; {dayjs().format('YYYY')} THAT&reg; All rights reserved.<br />
-					<em>v{version}</em>
-				</p>
+				<div class="mt-12 border-t border-gray-200 pt-8">
+					<p class="text-base text-gray-400 xl:text-center">
+						&copy; {dayjs().format('YYYY')} THAT&reg; All rights reserved.<br />
+						<em>v{version}</em>
+					</p>
+				</div>
 			</div>
 		</div>
 	</div>
---
+++ src/_components/newsletter/_HSForm.svelte
@@ -0,0 +1,29 @@
+<script>
+	import { onMount } from 'svelte';
+
+	function handleOnFormReady() {
+		// form html is the sent in
+		console.log('onFormReady Called');
+	}
+
+	onMount(() => {
+		// eslint-disable-next-line no-undef
+		hbspt.forms.create({
+			// required
+			region: 'na1',
+			portalId: '22769624',
+			formId: '7e7b1e14-711c-4b42-98aa-5eff05cb9786',
+			target: '#newsletterForm',
+			// optional
+			onFormReady: handleOnFormReady,
+			cssClass: 'inline-flex space-x-4 text-gray-500',
+			errorMessageClass: 'text-red-600 text-sm italic',
+			submitButtonClass:
+				'rounded-md border-2 border-thatBlue-500 bg-white px-4 py-2 text-base font-medium leading-6 text-thatBlue-500 shadow transition duration-150 ease-in-out hover:bg-thatBlue-500 hover:text-white focus:border-thatBlue-800 focus:bg-thatBlue-500 focus:text-white focus:outline-none focus:ring-thatBlue-500',
+			submitText: 'Join',
+			redirectUrl: '/my/notification-preferences/'
+		});
+	});
+</script>
+
+<div id="newsletterForm" />
