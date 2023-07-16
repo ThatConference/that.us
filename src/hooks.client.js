223ecb6734ae8c502086c8a8b485acee9450ea7d
Clark Sell <clark.sell@csell.net>
2023-07-16 08:40:53 -0500

    refactor: upgrades sentry, and fixes navigating store on server

--- src/hooks.client.js
+++ src/hooks.client.js
@@ -1,10 +1,13 @@
-// import * as Sentry from '@sentry/svelte';
+import * as Sentry from '@sentry/sveltekit';
 
-// export async function handleError({ error, event }) {
-// 	Sentry.captureException(error, { event });
+// If you don't want to use Session Replay, remove the `Replay` integration,
+// `replaysSessionSampleRate` and `replaysOnErrorSampleRate` options.
+Sentry.init({
+	dsn: 'https://15d4b436dc0a4366a0ac388c65772926@o235190.ingest.sentry.io/5357492',
+	tracesSampleRate: 1,
+	replaysSessionSampleRate: 0.1,
+	replaysOnErrorSampleRate: 1,
+	integrations: [new Sentry.Replay()]
+});
 
-// 	return {
-// 		message: 'Whoops!',
-// 		code: error.code ?? 'UNKNOWN'
-// 	};
-// }
+export const handleError = Sentry.handleErrorWithSentry();
