c6afaa684cbcca941b06862b376c1646487d9c29
Brett Slaski <brett@thatconference.com>
2023-07-19 16:54:15 -0500

    feat: enables fitler for family from url. Event schedule only

--- src/routes/events/(thatConferences)/wi/[year]/schedule/+page.js
+++ src/routes/events/(thatConferences)/wi/[year]/schedule/+page.js
@@ -1,12 +1,16 @@
 import sessionsApi from '$dataSources/api.that.tech/sessions';
 
-export async function load({ parent }) {
+export async function load({ parent, url }) {
 	const { querySessionsBySlug } = sessionsApi();
+
+	console.log('params', url);
+	let filterForFamily = url.searchParams.get('family') === 'true';
 
 	let { event } = await parent();
 	const sessions = await querySessionsBySlug({ slug: event.slug, pageSize: 250 });
 
 	return {
-		sessions
+		sessions,
+		filterForFamily
 	};
 }
