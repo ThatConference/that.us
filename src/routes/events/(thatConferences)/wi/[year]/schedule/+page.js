cf4568cd6fb9ec3cdddca4d5d0ec30de0f438e96
Brett Slaski <brett@thatconference.com>
2023-07-19 17:00:31 -0500

    chore: remove the console statement

--- src/routes/events/(thatConferences)/wi/[year]/schedule/+page.js
+++ src/routes/events/(thatConferences)/wi/[year]/schedule/+page.js
@@ -3,7 +3,6 @@ import sessionsApi from '$dataSources/api.that.tech/sessions';
 export async function load({ parent, url }) {
 	const { querySessionsBySlug } = sessionsApi();
 
-	console.log('params', url);
 	let filterForFamily = url.searchParams.get('family') === 'true';
 
 	let { event } = await parent();
